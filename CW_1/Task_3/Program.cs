c0ae7331db2500fd78ebdad83d8bada121022d44
*Elizarov <Super.elizarov1451@mail.ru>
2023-03-19 15:21:20 +0300

    1-я попытка написать программу к task_3

--- CW_1/Task_3/Program.cs
+++ CW_1/Task_3/Program.cs
@@ -1,3 +1,46 @@
 ﻿// Напишите программу, которая будет выдавать название дня недели
 // по заданному номеру.
 
+Console.Clear();
+
+int Day = int.Parse(Console.ReadLine()!);
+  
+if(Day == 1)
+{
+Console.WriteLine("Понедельник");
+}
+
+if(Day == 2)
+{
+Console.WriteLine("Вторник");
+}
+
+if(Day == 3)
+{
+Console.WriteLine("Среда");
+}
+
+if(Day == 4)
+{
+Console.WriteLine("Четверг");
+}
+
+if(Day == 5)
+{
+Console.WriteLine("Пятница");
+}
+
+if(Day == 6)
+{
+Console.WriteLine("Суббота");
+}
+
+if(Day == 7)
+{
+Console.WriteLine("Воскресенье");
+}
+
+else
+{
+Console.WriteLine("Ошибка");
+}
