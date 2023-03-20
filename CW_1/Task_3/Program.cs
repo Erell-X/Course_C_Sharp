2f6d631cde0147dabbf4643ab07b64be4ad2c780
*Elizarov <Super.elizarov1451@mail.ru>
2023-03-20 05:31:42 +0300

    Написание программы к task_3

--- CW_1/Task_3/Program.cs
+++ CW_1/Task_3/Program.cs
@@ -4,41 +4,53 @@
 Console.Clear();
 
 int Day = int.Parse(Console.ReadLine()!);
-  
-if(Day == 1)
-{
-Console.WriteLine("Понедельник");
-}
 
-if(Day == 2)
-{
-Console.WriteLine("Вторник");
-}
+  if(Day == 1)
+  {
+  Console.WriteLine("Понедельник");
+  }
 
-if(Day == 3)
-{
-Console.WriteLine("Среда");
-}
+else
 
-if(Day == 4)
-{
-Console.WriteLine("Четверг");
-}
+  if(Day == 2)
+  {
+  Console.WriteLine("Вторник");
+  }
 
-if(Day == 5)
-{
-Console.WriteLine("Пятница");
-}
+else
 
-if(Day == 6)
-{
-Console.WriteLine("Суббота");
-}
+  if(Day == 3)
+  {
+  Console.WriteLine("Среда");
+  }
 
-if(Day == 7)
-{
-Console.WriteLine("Воскресенье");
-}
+else
+
+  if(Day == 4)
+  {
+  Console.WriteLine("Четверг");
+  }
+
+else
+
+  if(Day == 5)
+  {
+  Console.WriteLine("Пятница");
+  }
+
+else
+
+  if(Day == 6)
+  {
+  Console.WriteLine("Суббота");
+  }
+
+else
+
+  if(Day == 7)
+  {
+  Console.WriteLine("Воскресенье");
+  }
 
 else
 {
