ec7d9ce46d98b470d0a2a967bdcb208d68ab244b
*Elizarov <Super.elizarov1451@mail.ru>
2023-03-22 05:50:00 +0300

    Написание программы к Task_4

--- CW_1/Task_4/Program.cs
+++ CW_1/Task_4/Program.cs
@@ -1,3 +1,22 @@
 ﻿// Напишите программу, которая на вход принимает одно число (N),
 // а на выходе показывает все целые числа в промежутке от -N до N.
 
+Console.Clear();
+
+int R = int.Parse(Console.ReadLine()!);
+
+int K = -R;
+
+  while(K<R)
+
+  {
+  Console.Write(K);
+  Console.Write(", ");
+  K++;
+  }
+
+  if(K==R)
+  {
+  Console.Write(K);
+  Console.Write(".");
+  }
